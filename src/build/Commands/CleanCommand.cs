10be94e775c65ec03a60027140dcdb0bbaa502a0
Yuriy <yuriy.gettya@elastic.co>
2019-10-16 21:23:36 -0500

    Added clean command.
    Deletes bin directory recursively in this impl.

---
+++ src/build/Commands/CleanCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+using Elastic.Installer;
+
+namespace ElastiBuild.Commands
+{ 
+    [Verb("clean", HelpText = "Clean downloaded .zip, temporary and output files")]
+    public class CleanCommand
+        : IElastiBuildCommand
+    {
+        public Task RunAsync(BuildContext ctx_)
+        {
+            Directory.Delete(ctx_.BinDir, true);
+
+            // TODO: Add support for Targets
+
+            return Task.CompletedTask;
+        }
+    }
+}
