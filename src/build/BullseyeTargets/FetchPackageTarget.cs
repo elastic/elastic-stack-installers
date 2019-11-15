5b7ff1cfc74f59b063a6b2899c1811f60c965d89
Yuriy <yuriy.gettya@elastic.co>
2019-11-15 11:33:04 -0600

    Clean up using statements

--- src/build/BullseyeTargets/FetchPackageTarget.cs
+++ src/build/BullseyeTargets/FetchPackageTarget.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using ElastiBuild.Commands;
 using ElastiBuild.Infra;
-using Elastic.Installer;
 
 namespace ElastiBuild.BullseyeTargets
 {
