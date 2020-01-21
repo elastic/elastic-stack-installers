75457b1d29f5a3afdce5b0faa2cef45d53791b0d
David Olaru <dolaru@elastic.co>
2020-01-21 13:14:39 +0000

    Add QA project + silent MSI installer tests

---
+++ src/qa/Core/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elastic.Installer;
+
+namespace QA.Core
+{
+    public static class Constants
+    {
+        public static class Dirs
+        {
+            public static readonly string TestResults = "test-results";
+            public static readonly string TestLogs = "test-logs";
+        }
+    }
+}
---
+++ src/qa/Core/PathsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Elastic.Installer;
+
+namespace QA.Core
+{
+    public class PathsProvider : CommonPathsProvider
+    {
+        public string TestResultsDir => lazyTestResultsDir.Value;
+        protected static readonly Lazy<string> lazyTestResultsDir = new Lazy<string>(
+            () => Path.Combine(lazyBinDir.Value, Constants.Dirs.TestResults));
+
+        public string TestLogsDir => lazyTestLogsDir.Value;
+        protected static readonly Lazy<string> lazyTestLogsDir = new Lazy<string>(
+            () => Path.Combine(lazyTestResultsDir.Value, Constants.Dirs.TestLogs));
+
+        public string ElasticImmutableRoot => lazyElasticImmutableRoot.Value;
+        protected static readonly Lazy<string> lazyElasticImmutableRoot = new Lazy<string>(
+            () => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles), "Elastic"));
+
+        public string ElasticMutableRoot => lazyElasticMutableRoot.Value;
+        protected static readonly Lazy<string> lazyElasticMutableRoot = new Lazy<string>(
+            () => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Elastic"));
+    }
+}
