a566c800263bf0416dd6ccd2dec3040c4d61980f
Yuriy <51912343+ygel@users.noreply.github.com>
2020-05-05 08:00:32 -0500

    Backslash is already part of %~dp0

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ set DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 rem When built with Release Manager we put 
 rem Net Core SDK in the parent dir
-set PATH=%PATH%;%~dp0\..\dotnet-sdk;
+set PATH=%PATH%;%~dp0..\dotnet-sdk;
 
 pushd .
 cd %~dp0%
