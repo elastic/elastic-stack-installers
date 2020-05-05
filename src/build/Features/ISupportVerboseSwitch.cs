047d5f709b72c55bc86454088b85cfe0377e2c23
Yuriy <51912343+ygel@users.noreply.github.com>
2020-05-05 08:01:01 -0500

    Added --verbose to ShowCommand

---
+++ src/build/Features/ISupportVerboseSwitch.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace ElastiBuild.Commands
+{
+    public interface ISupportVerboseSwitch
+    {
+        [Option("verbose", Default = false, HelpText = "Try to produce more verbose output.")]
+        bool VerboseSwitch { get; set; }
+    }
+}
