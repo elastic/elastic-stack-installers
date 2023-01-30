086bf143097aadffb9be0e749588e3bf3dd5ed4e
Alpar Torok <alpar.torok@elastic.co>
2023-01-30 16:02:36 +0200

    fix

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -4,8 +4,6 @@ echo "~~~ Enable IE"
 dism /online /Add-Capability /CapabilityName:Browser.InternetExplorer~~~~0.0.11.0
 dism /online /Enable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 
-#Check Script is running with Elevated Privileges
-Check-RunAsAdministrator
 
 echo "~~~ Installing dotnet-sdk"
 & "./tools/dotnet-install.ps1" -NoPath -JSonFile global.json -Architecture "x64" -InstallDir c:/dotnet-sdk
