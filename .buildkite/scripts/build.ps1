931acb878b49917d825db6b625da1beea4a6dcb0
Alpar Torok <alpar.torok@elastic.co>
2023-01-30 15:27:35 +0200

    Enable IE

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,36 +1,7 @@
 $stack_version="8.7.0"
 
-echo "~~~ Run as Administrator"
-# From https://www.sharepointdiary.com/2015/01/run-powershell-script-as-administrator-automatically.html
-Function Check-RunAsAdministrator()
-{
-  #Get current user context
-  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-
-  #Check user is running the script is member of Administrator Group
-  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
-  {
-       Write-host "Script is running with Administrator privileges!"
-  }
-  else
-    {
-       #Create a new Elevated process to Start PowerShell
-       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-       # Specify the current script path and name as a parameter
-       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
-
-       #Set the Process to elevated
-       $ElevatedProcess.Verb = "runas"
-
-       #Start the new elevated process
-       [System.Diagnostics.Process]::Start($ElevatedProcess)
-
-       #Exit from the current, unelevated, process
-       Exit
-
-    }
-}
+echo "~~~ Enable IE"
+dism /online /Add-Capability /CapabilityName:Browser.InternetExplorer~~~~0.0.11.0.
 
 #Check Script is running with Elevated Privileges
 Check-RunAsAdministrator
