0d4b7282b7c3bd8aeb8d709bdd575d302e268562
David Natachanny <David.Natachanny@elastic.co>
2023-02-27 20:03:23 +0200

    add commented out conditionals

--- .buildkite/scripts/dra-publish.sh
+++ .buildkite/scripts/dra-publish.sh
@@ -20,24 +20,38 @@ export DRA_CREDS
 VERSION=$(cat Directory.Build.props | awk -F'[><]' '/<StackVersion>/{print $3}' | tr -d '[:space:]')
 export VERSION
 
-export WORKFLOW="snapshot"
-
 # Publish DRA artifacts for both snapshot and staging
-echo "+++ Publishing DRA artifacts..."
+
+function run_release_manager() {
+    echo "+++ Publishing $BUILDKITE_BRANCH ${WORKFLOW} DRA artifacts..."
     docker run --rm \
-    --name release-manager \
-    -e VAULT_ADDR="$(echo "$DRA_CREDS" | jq -r '.vault_addr')" \
-    -e VAULT_ROLE_ID="$(echo "$DRA_CREDS" | jq -r '.role_id')" \
-    -e VAULT_SECRET_ID="$(echo "$DRA_CREDS" | jq -r '.secret_id')" \
-    --mount type=bind,readonly=false,src="$PWD",target=/artifacts \
-    docker.elastic.co/infra/release-manager:latest \
+        --name release-manager \
+        -e VAULT_ADDR="${VAULT_ADDR}" \
+        -e VAULT_ROLE_ID="${VAULT_ROLE_ID}" \
+        -e VAULT_SECRET_ID="${VAULT_SECRET_ID}" \
+        --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
+        docker.elastic.co/infra/release-manager:latest \
         cli collect \
         --project elastic-stack-installers \
-        --branch "$BUILDKITE_BRANCH" \
-        --commit "$BUILDKITE_COMMIT" \
-        --workflow "$WORKFLOW" \
-        --version "$VERSION" \
+        --branch "${BUILDKITE_BRANCH}" \
+        --commit "${BUILDKITE_COMMIT}" \
+        --workflow "${WORKFLOW}" \
+        --version "${VERSION}" \
         --artifact-set main \
         --dry-run
+}
+
+export WORKFLOW="snapshot"
+run_release_manager
+# if [[ $BUILDKITE_BRANCH == "main" ]]; then
+#     WORKFLOW="snapshot"
+#     run_release_manager
+# else
+#     WORKFLOW="staging"
+#     run_release_manager
+#     WORKFLOW="snapshot"
+#     run_release_manager
+# fi
+
 
 # I can run snapshot on main+both on version branches and use a matrix step.
