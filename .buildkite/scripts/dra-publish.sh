6497d015e24e888395e67a99f1c14b45df96cd2e
David Natachanny <David.Natachanny@elastic.co>
2023-03-02 11:47:16 +0200

    test with main brach

--- .buildkite/scripts/dra-publish.sh
+++ .buildkite/scripts/dra-publish.sh
@@ -36,8 +36,7 @@ function run_release_manager() {
         docker.elastic.co/infra/release-manager:latest \
         cli collect \
         --project elastic-stack-installers \
-        # --branch "${BUILDKITE_BRANCH}" \
-        --branch main
+        --branch main \
         --commit "${BUILDKITE_COMMIT}" \
         --workflow "${WORKFLOW}" \
         --version "${VERSION}" \
