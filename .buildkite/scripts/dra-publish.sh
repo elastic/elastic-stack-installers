6c269650b0abd9fdef492483dd136c503e7f3d8a
David Natachanny <david.natachanny@elastic.co>
2023-03-06 19:04:18 +0200

    Stack installers dra pipeline (#110)Co-authored-by: Alpar Torok <alpar.torok@elastic.co>
    * Create a buildkite pipeline for the build
    
    * Build script that actually builds something
    
    * fix typo
    
    * Remove old jobs, add notifications,build schedule and badge
    
    * Oh, by the way, did I ever tell you ...
    
    * ... wait, you must hear hear it!
    
    * There I was finally being able to replace this Jenkins job ...
    
    * .. with this lovely Buildkite job, but had trouble with the powershell syntax
    
    * It's a bit tricky, you know, as I'm working on a mac so I can't try it out locally
    
    * How come on a mac? Yea, I'we been on Linux for more than a decade
    
    * Fair question, but I can't finish my story if you keep intrerupting
    
    * in short, it's because of the battery life
    
    * every now and then if the weather is nice ...
    
    * Oh! see what you did there? Back to our storry
    
    * So I was working on this BK pipeline that ran on a windows CI worker
    
    * and the strangest thing happened
    
    * It worked on the first try
    
    * It was  simple, but how often does that happen?
    
    * Try installing newer .netwqa
    
    * 4.5
    
    * 4.5.2
    
    * single script
    
    * remove show
    
    * 4.7.2
    
    * fix exit code
    
    * annother trywqa
    
    * 5.0.408
    
    * Install both sdks
    
    * Install both
    
    * reverse order
    
    * move to 45
    
    * typo
    
    * bump deps
    
    * all on 3
    
    * both again
    
    * try
    
    * Try download missing assemblies
    
    * add more time servers
    
    * create idr
    
    * run as admin
    
    * Enable IE
    
    * enable IEwqa
    
    * fix
    
    * Don't eat the exception
    
    * fix compile
    
    * run build as admin
    
    * don't use start-process
    
    * Make sure msi is writable
    
    * fix
    
    * fix compile
    
    * remove enable ie
    
    * log events
    
    * add all
    
    * grab the log only
    
    * no executable
    
    * event log is not usefull
    
    * fix compile
    
    * try a delay
    
    * compile
    
    * process monitor the build
    
    * typo
    
    * fix wait
    
    * try to take ownership
    
    * find the handlewqa
    
    * -
    
    * more debug
    
    * -
    
    * Try to sign a copy of the file
    
    * -
    
    * -
    
    * -
    
    * fix command
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * deal with releae branches
    
    * initial dra pipeline commit
    
    * add step keys and dra publish file
    
    * add step keys and dra publish file
    
    * add download from specific step
    
    * add change in url
    
    * remove download verification step
    
    * change bind mounting to be correct
    
    * add commented out conditionals
    
    * remove bin flatten nested dir
    
    * remove typo
    
    * export vault args
    
    * test with main brach
    
    * test with main brach
    
    * bin out list
    
    * update version to 8.8.0 on branch
    
    * remove double dir invocation
    
    * remove dir specification
    
    * artifacts download path
    
    * artifacts download path
    
    * add conditional to only run branch on main and version branches
    
    * add case in for branches and prs
    
    * change regex for branches + add cat command to ps1 script
    
    * fallback on stack version hardcoded
    
    * remove dir list
    
    * Revert "update version to 8.8.0 on main"
    
    This reverts commit 220cfd6e762968df37f1debb2284ec1522cf2055.
    
    * change version to 8.8.0
    
    ---------
    
    Co-authored-by: Alpar Torok <alpar.torok@elastic.co>

---
+++ .buildkite/scripts/dra-publish.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -uo pipefail
+
+set +x
+
+# Download artifacts from Buildkite "Build stack installers" step
+echo "+++ Downloading artifacts..."
+buildkite-agent artifact download 'bin\out\**\*.msi' . --step build
+chmod -R 777 bin/out
+
+echo "+++ Setting DRA params" 
+
+# Shared secret path containing the dra creds for project teams
+DRA_CREDS=$(vault kv get -field=data -format=json kv/ci-shared/release/dra-role)
+VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr')
+VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id')
+VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id') 
+export VAULT_ADDR VAULT_ROLE_ID VAULT_SECRET_ID
+
+# Retrieve version value
+VERSION=$(cat Directory.Build.props | awk -F'[><]' '/<StackVersion>/{print $3}' | tr -d '[:space:]')
+export VERSION
+
+# Publish DRA artifacts for both snapshot and staging
+
+function run_release_manager() {
+    echo "+++ Publishing $BUILDKITE_BRANCH ${WORKFLOW} DRA artifacts..."
+    docker run --rm \
+        --name release-manager \
+        -e VAULT_ADDR="${VAULT_ADDR}" \
+        -e VAULT_ROLE_ID="${VAULT_ROLE_ID}" \
+        -e VAULT_SECRET_ID="${VAULT_SECRET_ID}" \
+        --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
+        docker.elastic.co/infra/release-manager:latest \
+        cli collect \
+        --project elastic-stack-installers \
+        --branch "${BRANCH}" \
+        --commit "${BUILDKITE_COMMIT}" \
+        --workflow "${WORKFLOW}" \
+        --version "${VERSION}" \
+        --artifact-set main \
+        ${BUILDKITE_PULL_REQUEST:+--dry-run} \
+        #
+}
+
+case "$BUILDKITE_BRANCH" in
+main)
+    WORKFLOW="snapshot"
+    BRANCH="main"
+    run_release_manager
+    ;;
+^[0-9]+\.[0-9]+$)
+    BRANCH="$BUILDKITE_BRANCH"
+    WORKFLOW="staging"
+    run_release_manager
+    WORKFLOW="snapshot"
+    run_release_manager
+    ;;
+*) 
+    echo "Running in dry-run mode for $BUILDKITE_BRANCH"
+    BRANCH="main"
+    WORKFLOW="snapshot"
+    run_release_manager
+    ;;   
+esac
