4f4b2553995dc625364564eea2a34aa21c5d9096
David Natachanny <David.Natachanny@elastic.co>
2023-03-29 10:33:23 +0300

    remove revert

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 $stack_version="8.8.0"
+=======
+$stack_version="8.6.3"
+>>>>>>> parent of caa4e64 (Revert "8.6-dra-backport" (#121)
 
 echo "~~~ Installing dotnet-sdk"
 & "./tools/dotnet-install.ps1" -NoPath -JSonFile global.json -Architecture "x64" -InstallDir c:/dotnet-sdk
@@ -20,6 +24,7 @@ $client = new-object System.Net.WebClient
 $currentDir = $(Get-Location).Path
 $beats = @('auditbeat', 'filebeat', 'heartbeat', 'metricbeat', 'packetbeat', 'winlogbeat')
 $ossBeats = $beats | ForEach-Object { $_ + "-oss" }
+<<<<<<< HEAD
 $workflow = ${env:WORKFLOW}
 
 echo "~~~ downloading beat $workflow dependencies"
@@ -91,4 +96,75 @@ $expected = 2 * $beats.Length
 if ($msiCount -ne $expected) {
     Write-Error "Expected $expected msi executable to be produced, but $msiCount were"
     exit 1
+=======
+$kinds =  @("-SNAPSHOT")
+if ($env:BUILDKITE_BRANCH -match '^\d+\.\d+$') {
+   $kinds += "" # empty string for staging
+}
+foreach ($kind in $kinds) {
+    echo "~~~ downloading beat dependencies$kind"
+    Remove-Item bin/in -Recurse -Force -ErrorAction Ignore
+    New-Item bin/in -Type Directory -Force
+    $version = $stack_version + $kind
+    $response = Invoke-WebRequest -UseBasicParsing -Uri "https://artifacts-api.elastic.co/v1/versions/$version/builds/latest"
+    $json = $response.Content | ConvertFrom-Json
+    $buildId = $json.build.build_id
+    if ($kind -eq "") {
+        $hostname = "staging.elastic.co"
+    } else {
+        $hostname = "snapshots.elastic.co"
+    }
+    foreach ($beat in ($beats + $ossBeats)) {
+        try {
+            $beatName = $beat.Replace("-oss", "")
+            $url = "https://$hostname/$buildId/downloads/beats/$beatName/$beat-$version-windows-x86_64.zip"
+            echo "Downloading from $url"
+            $client.DownloadFile(
+               $url,
+               "$currentDir/bin/in/$beat-$version-windows-x86_64.zip"
+            )
+        }
+        catch [System.Net.WebException] {
+            if ($_.Exception.InnerException) {
+               Write-Error $_.Exception.InnerException.Message
+            } else {
+               Write-Error $_.Exception.Message
+            }
+            throw "An error was encountered while downloading dependencies, aborting."
+        }
+    }
+
+    echo "--- Building msi$kind"
+    $args = @(
+        "run",
+        "--project",
+        "src\build\ElastiBuild.csproj",
+        "-c",
+        "Release",
+        "--",
+        "build",
+        "--cid",
+        $version,
+        "--cert-file",
+        "$cert_home/msi_certificate.p12",
+        "--cert-pass",
+        "$cert_home/msi_password.txt"
+    )
+    $args += ($beats + $ossBeats)
+    &dotnet $args
+    if ($LastExitcode -ne 0) {
+        Write-Error "Build$kind failed with exit code $LastExitcode"
+        exit $LastExitcode
+    } else {
+        echo "Build$kind completed with exit code $LastExitcode"
+    }
+
+    echo "--- Checking that all artefacts are there"
+    $msiCount = Get-ChildItem bin/out -Include "*.msi" -Recurse | Measure-Object | Select-Object -ExpandProperty Count
+    $expected = 2 * $beats.Length
+    if ($msiCount -ne $expected) {
+        Write-Error "Expected $expected msi executable to be produced, but $msiCount were"
+        exit 1
+    }
+>>>>>>> parent of caa4e64 (Revert "8.6-dra-backport" (#121)
 }
