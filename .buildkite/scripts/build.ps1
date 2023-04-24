3e78e9b6b373a8fd5b38d7d968a7a1df43acede0
David Natachanny <David.Natachanny@elastic.co>
2023-04-24 17:51:30 +0300

    set foreach on stack_installers suffix

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -41,12 +41,14 @@ if ($workflow -eq "snapshot") {
 foreach ($beat in ($beats + $ossBeats)) {
     try {
         $beatName = $beat.Replace("-oss", "")
-        $url = "https://$prefix/downloads/beats/$beatName/$beat-$version-windows-x86.zip"
-        echo "Downloading from $url"
-        $client.DownloadFile(
-            $url,
-            "$currentDir/bin/in/$beat-$version-windows-x86_64.zip"
-        )
+        forceach ($bitsuffix in ("","_64")) {
+            $url = "https://$prefix/downloads/beats/$beatName/$beat-$version-windows-x86$bitsuffix.zip"
+            echo "Downloading from $url"
+            $client.DownloadFile(
+                $url,
+                "$currentDir/bin/in/$beat-$version-windows-x86_64.zip"
+            )
+        }
     }
     catch [System.Net.WebException] {
         if ($_.Exception.InnerException) {
