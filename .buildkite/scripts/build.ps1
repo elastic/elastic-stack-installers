5fa8e164653e7d29dd9de9e1fcd070d86b5b29c1
Navya Uppalapati <119856217+navyau09@users.noreply.github.com>
2023-06-08 16:42:00 +0200

    bump to 8.8.2 (#149)

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,4 +1,4 @@
-$stack_version="8.8.1"
+$stack_version="8.8.2"
 
 echo "~~~ Installing dotnet-sdk"
 & "./tools/dotnet-install.ps1" -NoPath -JSonFile global.json -Architecture "x64" -InstallDir c:/dotnet-sdk
