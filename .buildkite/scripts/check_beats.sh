306e90664013870d74dcd0ee05abce3d6dd6c077
Alpar Torok <alpar.torok@elastic.co>
2023-07-14 14:04:58 +0300

    Add script to compare manifest

--- .buildkite/scripts/check_beats.sh
+++ .buildkite/scripts/check_beats.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+BRANCH="${BRANCH:-$BUILDKITE_BRANCH}"
+
+BEATS_MANIFEST=$(curl  https://artifacts-staging.elastic.co/beats/latest/$BRANCH.json | jq -r '.manifest_url')
+INSTALLERS_MANIFEST=$(curl  https://artifacts-staging.elastic.co/elastic-stack-installers/latest/$BRANCH.json | jq -r '.manifest_url')
+INSTALLERS_BEATS_DEPENDENCY=$(curl $INSTALLERS_MANIFEST | jq -r '.projects.elasticsearch.dependencies[] | select(.prefix == "beats") | .build_uri')
+
+if [ "$BEATS_MANIFEST" = "$INSTALLERS_BEATS_DEPENDENCY" ]
+then
+   echo "We have the latest beats! Nothing to do" >&2
+else
+   echo "Need to trigger a build, $BEATS_MANIFEST available but ES has $INSTALLERS_BEATS_DEPENDENCY" > &2
+fi
+
 echo "steps:"
