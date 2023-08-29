0a2ba70a7ee27297f9952aad2c251e0586c1fda7
David Natachanny <David.Natachanny@elastic.co>
2023-08-29 18:41:18 +0300

    bump version 8.9.3

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,4 +1,4 @@
-$stack_version="8.9.2"
+$stack_version="8.9.3"
 
 echo "~~~ Installing dotnet-sdk"
 & "./tools/dotnet-install.ps1" -NoPath -JSonFile global.json -Architecture "x64" -InstallDir c:/dotnet-sdk
