cb795a58944e4799e35366a54718d27f2290acd3
Julien Lind <julien.lind@elastic.co>
2023-10-05 09:48:04 +0200

    Bump version to 8.12 (#185)
    * Update build.ps1
    
    * Update catalog-info.yaml

--- .buildkite/scripts/build.ps1
+++ .buildkite/scripts/build.ps1
@@ -1,4 +1,4 @@
-$stack_version="8.11.0"
+$stack_version="8.12.0"
 
 echo "~~~ Installing dotnet-sdk"
 & "./tools/dotnet-install.ps1" -NoPath -JSonFile global.json -Architecture "x64" -InstallDir c:/dotnet-sdk
