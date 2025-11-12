eeaa02e1428989fca8a59055718c6d6119e55bcd
Doug Weyrauch <douglas.weyrauch@elastic.co>
2025-11-12 18:19:55 -0500

    Clean up new option

--- src/build/Features/ISupportArchitecture.cs
+++ src/build/Features/ISupportArchitecture.cs
@@ -7,23 +7,13 @@ namespace ElastiBuild.Commands
         public static class ISupportArchitecture
         {
             public static string Architecture =>
-                "Architecture. x86_64 or arm64";
+                "Architecture. x86_64 or arm64. Default is x86_64";
         }
     }
 
     public interface ISupportArchitecture
     {
-        // TODO: arch latest
         [Option("arch", Default = "x86_64", HelpText = nameof(Architecture), ResourceType = typeof(Resources.ISupportArchitecture))]
         string Architecture { get; set; }
     }
-
-/*
-    public interface ISupportRequiredArchitecture
-    {
-        // TODO: arch latest
-        [Option("arch", Required = true, HelpText = nameof(Architecture), ResourceType = typeof(Resources.ISupportArchitecture))]
-        string Architecture { get; set; }
-    }
-*/
 }
