a2efa4c2ace1838ef7729804e79ade16384b5522
Doug W <55553826+dwhyrock@users.noreply.github.com>
2025-12-11 09:05:20 -0500

    [9.2] [Agent] Add ability to build MSI for elastic-agent for arm64 (#343) (#344)
    [Agent] Add ability to build MSI for elastic-agent for arm64 (#343)
    
    * Adding windows arm64 to stack installers build script
    
    * Add temp set to 9.2 branch
    
    * Is64Bit include arm64
    
    * Update magic strings arch
    
    * Build MSI for arm64 Agent as well as x64
    
    * Stick to x86_64 for testing
    
    * Clean up new option
    
    * Undo temp changes
    
    * Temp set branch to 9.2
    
    * Remove temp test setting
    
    (cherry picked from commit 89de33a7d8962aa345312b61a11829b95fe7dbfe)

--- .buildkite/scripts/test.ps1
+++ .buildkite/scripts/test.ps1
@@ -9,13 +9,13 @@ write-host "`$env:AGENT = $($Env:AGENT)"
 
 if ($psversiontable.psversion -lt "7.4.0") {
     # Download Powershell Core, and rerun this script using Powershell Core
-    
+
     write-host "Downloading Powershell Core"
     invoke-webrequest -uri https://github.com/PowerShell/PowerShell/releases/download/v7.4.0/PowerShell-7.4.0-win-x64.zip -outfile pwsh.zip
-    
+
     write-host "Expanding Powershell Core"
     Expand-Archive pwsh.zip -destinationpath (Join-Path $PSScriptRoot "pwsh")
-    
+
     Write-host "Invoking from Powershell Core"
     & (Join-Path $PSScriptRoot "pwsh/pwsh.exe") -file (Join-Path $PSScriptRoot "test.ps1")
 
@@ -28,7 +28,8 @@ if ($psversiontable.psversion -lt "7.4.0") {
     }
 }
 
-$AgentMSI = Get-ChildItem bin/out -Include "elastic-agent*.msi" -Recurse
+# Default to x86_64 for now until Windows/arm64 testing is added
+$AgentMSI = Get-ChildItem bin/out -Include "elastic-agent*x86_64*.msi" -Recurse
 
 if ($AgentMSI -eq $null) {
     write-error "No agent MSI found to test"
